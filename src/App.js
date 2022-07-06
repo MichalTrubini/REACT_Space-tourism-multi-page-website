22a355b80335cfd83fc2d8d66b3e0590a769109d
Michal Trubini <mchl.trubini@gmail.com>
2022-07-06 15:57:56 +0200

    added lazy loading

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import {Route} from 'react-router-dom';
 
 import Header from "./components/Header";
-import Home from './pages/Home';
-import Destination from './pages/Destination';
-import Crew from './pages/Crew';
-import Technology from './pages/Technology';
+//import Home from './pages/Home';
+//import Destination from './pages/Destination';
+//import Crew from './pages/Crew';
+//import Technology from './pages/Technology';
+
+const Home = React.lazy(() => import('./pages/Home'));
+const Destination = React.lazy(() => import('./pages/Destination'));
+const Crew = React.lazy(() => import('./pages/Crew'));
+const Technology = React.lazy(() => import('./pages/Technology'));
 
 function App() {
   return (
