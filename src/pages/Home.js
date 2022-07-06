3e0f84f8141d5819d7d19bb9d8d19b8a0f140e78
Michal Trubini <mchl.trubini@gmail.com>
2022-07-06 17:27:34 +0200

    Link fix on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,5 @@
+import {Link} from 'react-router-dom';
+
 import '../css/home.css'
 
 const Home = () => {
@@ -13,7 +15,7 @@ const Home = () => {
                 <p className="home__about">Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
             </div>
             <div className="home__link-container">
-                <a href="something" className="home__link">Explore</a>
+                <Link to="/destination" className="home__link">Explore</Link>
             </div>
         </div>
     )
